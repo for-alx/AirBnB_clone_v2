b2eb72c416bbe3e1e867bf749f8a38334d770792
Aymen <aymenabduseid10@gmail.com>
2023-03-20 23:04:55 +0300

    add comment

--- setup_mysql_test.sql
+++ setup_mysql_test.sql
@@ -2,6 +2,8 @@
 -- Create db and user
 CREATE DATABASE IF NOT EXISTS hbnb_test_db;
 CREATE USER IF NOT EXISTS 'hbnb_test'@'localhost' IDENTIFIED BY 'hbnb_test_pwd';
+
+-- Granting a User Permissions
 GRANT ALL PRIVILEGES ON hbnb_test_db.* TO 'hbnb_test'@'localhost';
 GRANT SELECT ON performance_schema.* TO 'hbnb_test'@'localhost';
 FLUSH PRIVILEGES;
