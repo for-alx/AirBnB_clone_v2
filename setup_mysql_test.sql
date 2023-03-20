c52bfbbb4553b0716f6f98ad95c37e34ee3ab4d3
Aymen <aymenabduseid10@gmail.com>
2023-03-20 23:02:48 +0300

    add comment

--- setup_mysql_test.sql
+++ setup_mysql_test.sql
@@ -7,3 +7,4 @@ CREATE USER IF NOT EXISTS 'hbnb_test_db'@'localhost' IDENTIFIED BY 'hbnb_test_pw
 GRANT USAGE ON *.* TO 'hbnb_test'@'localhost';
 GRANT SELECT ON `performance_schema`.* TO 'hbnb_test'@'localhost';
 GRANT ALL PRIVILEGES ON `hbnb_test_db`.* TO 'hbnb_test'@'localhost';
+FLUSH PRIVILEGES;
