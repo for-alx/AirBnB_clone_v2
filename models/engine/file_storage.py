d724999d451e2c76fb016e84f68c18b2782fc268
Aymen <aymenabduseid10@gmail.com>
2023-03-21 10:22:34 +0300

    fix over-indented

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -10,7 +10,16 @@ class FileStorage:
 
     def all(self, cls=None):
         """Returns a dictionary of models currently in storage"""
-        return FileStorage.__objects
+        if cls is not None:
+            fs_dict = {}
+            for key, value in self.__objects.items():
+                if cls == value.__class__ or cls == value.__class__.__name__:
+                    fs_dict[key] = value
+            return fs_dict
+        return self.__objects
+        # return FileStorage.__objects
+        # # print(self.__objects)
+
 
     def new(self, obj):
         """Adds new object to storage dictionary"""
