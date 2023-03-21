b53df323bc03a77e21d31de3cc993dbda280a0cf
Mr-robot <hackingtest017@gmail.com>
2023-03-21 22:18:27 +0300

    task 8

--- models/user.py
+++ models/user.py
@@ -11,9 +11,11 @@ class User(BaseModel, Base):
     # password = ''
     # first_name = ''
     # last_name = ''
+    # reviews = relationship("Review", backref="user", cascade="delete")
     __tablename__ = 'users'
 
     email = Column(String(128), nullable=False)
     password = Column(String(128), nullable=False)
     first_name = Column(String(128))
     last_name = Column(String(128))
+    places = relationship('Place', backref='user', cascade="delete")
