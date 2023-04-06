73aa584cbdc8507f581e3de290b710d1ef3f412e
Mr-robot <hackingtest017@gmail.com>
2023-04-06 20:19:44 +0300

    add bash file

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -1,13 +1,15 @@
 #!/usr/bin/env bash
 # Prepare web servers for static web-page
 
-package_name='nginx'
+# package_name='nginx'
 
-status="$(dpkg-query -W --showformat='${db:Status-Status}' "$package_name" 2>&1)"
-if [ ! "$status" = 'installed' ]; then
-	apt-get update
-	apt-get install -y nginx
-fi
+# status="$(dpkg-query -W --showformat='${db:Status-Status}' "$package_name" 2>&1)"
+# if [ ! "$status" = 'installed' ]; then
+# 	apt-get update
+# 	apt-get install -y nginx
+# fi
+apt-get update
+apt-get install -y nginx
 
 mkdir -p /data/web_static/shared /data/web_static/releases/test
 
