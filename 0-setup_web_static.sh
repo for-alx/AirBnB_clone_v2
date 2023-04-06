bc0773ee3d3462931ce98813a92bcfbd82fb3e7e
Mr-robot <hackingtest017@gmail.com>
2023-04-06 20:25:48 +0300

    add bash file

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -8,18 +8,12 @@
 # 	apt-get update
 # 	apt-get install -y nginx
 # fi
-apt-get update
-apt-get install -y nginx
-
-mkdir -p /data/web_static/shared /data/web_static/releases/test
-
-echo "Wala" > /data/web_static/releases/test/index.html
-
-ln -sf /data/web_static/releases/test /data/web_static/current
-
-chown -Rh ubuntu:ubuntu /data
-
-
-sed -i '33i\\tlocation /hbnb_static/ {\n\t\talias /data/web_static/current/;\n\t}\n' /etc/nginx/sites-available/default
-
-nginx -s reload
+apt-get update && \
+apt-get install -y nginx && \
+mkdir -p -m=755 /data/web_static/{releases/test,shared} || exit 0
+echo 'Testing 123' > /data/web_static/releases/test/index.html
+ln -sf /data/web_static/releases/test/ /data/web_static/current
+chown -hR ubuntu:ubuntu /data/
+insert='\\tlocation /hbnb_static/ {\n\t\talias /data/web_static/current/;}'
+sed -i "37i $insert" /etc/nginx/sites-available/default
+service nginx restart
