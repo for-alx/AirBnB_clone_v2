fd260aeb0c7b3dfdf83cc8bf8b794104cb097562
Mr-robot <hackingtest017@gmail.com>
2023-04-06 22:56:52 +0300

    :)

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -20,11 +20,13 @@ def do_deploy(archive_path):
         file_name = archive_name.split('.')[0]
         des = "/data/web_static/releases/"
 
-        put(archive_path, "/tmp/")
+        put(archive_path, '/tmp/')
         run('mkdir -p {}{}/'.format(des, file_name))
-        run("tar -xzvf /tmp/{} -C {}{}".format(archive_name, des, file_name))
-        run("rm -fr /tmp/{}".format(archive_name))
-        run("rm -rf /data/web_static/current")
+        run('tar -xzf /tmp/{} -C {}{}/'.format(archive_name, des, file_name))
+        run('rm /tmp/{}'.format(archive_name))
+        run('mv {0}{1}/web_static/* {0}{1}/'.format(des, file_name))
+        run('rm -rf {}{}/web_static'.format(des, file_name))
+        run('rm -rf /data/web_static/current')
         run('ln -s {}{}/ /data/web_static/current'.format(des, file_name))
         return True
     except Exception:
