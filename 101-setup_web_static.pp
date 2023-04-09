ff77601cce5fb72950be944675ff0cd5393edcc1
Mr-robot <hackingtest017@gmail.com>
2023-04-09 19:20:49 +0300

    final fix

--- 101-setup_web_static.pp
+++ 101-setup_web_static.pp
@@ -1,5 +1,13 @@
 # Configures a web server for deployment.
 
+$index_file = "<html>
+  <head>
+  </head>
+  <body>
+    Holberton School
+  </body>
+</html>
+"
 
 file { ['/data', '/data/web_static', '/data/web_static/shared', '/data/web_static/releases', '/data/web_static/releases/test']:
   ensure => 'directory',
@@ -9,7 +17,7 @@ file { ['/data', '/data/web_static', '/data/web_static/shared', '/data/web_stati
 
 file { '/data/web_static/releases/test/index.html':
   ensure  => 'present',
-  content => "Holberton School Puppet\n",
+  content => $index_file,
   owner   => 'ubuntu',
   group   => 'ubuntu',
 }
