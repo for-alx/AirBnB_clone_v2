ac8833850880660382883536b6b1d0dababf923e
Mr-robot <hackingtest017@gmail.com>
2023-04-21 02:28:28 -0500

    add number function

---
+++ web_flask/4-number_route.py
@@ -0,0 +1,54 @@
+#!/usr/bin/python3
+"""
+    Basic falsk web application
+"""
+from flask import Flask, escape
+
+
+app = Flask(__name__)
+app.url_map.strict_slashes = False
+
+
+@app.route('/')
+def index():
+    """
+        web root content
+    """
+    return 'Hello HBNB!'
+
+
+@app.route('/hbnb')
+def hbnb():
+    """
+        function for /hbnb url
+    """
+    return 'HBNB'
+
+
+@app.route('/c/<text>')
+def c(text):
+    """
+        accepting text input
+    """
+    return 'C %s' % escape(text.replace("_", " "))
+
+
+@app.route('/python/')
+@app.route('/python/<text>')
+def python(text='is cool'):
+    """
+        Python function
+    """
+    return 'Python %s' % escape(text.replace("_", " "))
+
+
+@app.route('/number/<int:n>')
+def number(n):
+    """
+        accepting url parameter(number)
+    """
+    return '%d is a number' % n
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
