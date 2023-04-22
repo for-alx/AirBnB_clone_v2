ff666a7ca99d90171def4d68484f1697d3e137ce
Mr-robot <hackingtest017@gmail.com>
2023-04-22 07:47:56 -0500

    task 9 first commit

--- web_flask/9-states.py
+++ web_flask/9-states.py
@@ -10,8 +10,32 @@ app = Flask(__name__)
 app.url_map.strict_slashes = False
 
 
+@app.route('/states')
+def states():
+    """
+        list of all State objects present
+    """
+    states = list(storage.all('State').values())
+    return render_template('7-states_list.html', tasks=states)
 
 
+@app.route('/states/<id>')
+def states_by_id(id):
+    """
+        list of all cities if id exist
+    """
+    temp = []
+    states = list(storage.all('State').values())
+    print("======================")
+    for state in states:
+        if state.id == id:
+            temp.append(state)
+            return render_template(
+                '9-states.html', tasks=temp, name=state.name)
+    return render_template('9-states.html', tasks=None)
+
+
+@app.teardown_appcontext
 def teardown(self):
     """
         After each request you must remove the current session
